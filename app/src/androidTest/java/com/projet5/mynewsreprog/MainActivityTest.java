4720b879731e96a1dbc9ee699b7110c6d67457e0
yass <44866278+yacineOC@users.noreply.github.com>
2020-10-26 16:43:10 +0100

    Update 2

--- app/src/androidTest/java/com/projet5/mynewsreprog/MainActivityTest.java
+++ app/src/androidTest/java/com/projet5/mynewsreprog/MainActivityTest.java
@@ -1,6 +1,4 @@
 package com.projet5.mynewsreprog;
-
-import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -12,8 +10,6 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withChild;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
